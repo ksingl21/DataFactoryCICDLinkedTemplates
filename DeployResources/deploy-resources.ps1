673724a83f3f0ca55efdd0988ecd04e78b8d2387
Nick Ludwig <nick.ludwig223@gmail.com>
2024-06-09 14:29:02 -0500

    Removing test pipelines

--- DeployResources/deploy-resources.ps1
+++ DeployResources/deploy-resources.ps1
@@ -14,21 +14,21 @@ $Location = 'eastus'
 
 
 # Create the DEV, UAT, PROD resource group names
-az group create --name $DEVResourceGroupName --location $Location # DEV
-az group create --name $UATResourceGroupName --location $Location # UAT
-az group create --name $PRODResourceGroupName --location $Location # PROD
+# az group create --name $DEVResourceGroupName --location $Location # DEV
+# az group create --name $UATResourceGroupName --location $Location # UAT
+# az group create --name $PRODResourceGroupName --location $Location # PROD
 
 # Create a Data Factory
-az datafactory create --factory-name $DataFactoryName --resource-group $ResourceGroupName --location $Location
+az datafactory create --factory-name $DataFactoryName --resource-group $DEVResourceGroupName --location $Location
 
 
 # Creates 200 new Data Factory pipelines (35 Wait Activities in each pipeline)
 # Loops from 1 to 200 creating a pipeline named PL_WAIT_Number. Ex: PL_WAIT_1, PL_WAIT_2... PL_WAIT_200 
-for ($i = 1; $i -le 200; $i++) {
+for ($i = 1; $i -le 10; $i++) {
     $PipelineName = "PL_WAIT_" + $i.ToString() # Ex: PL_WAIT_1
     
     # Creates the new Data Factory Pipeline using the 35 Wait Activity JSON file DataFactoryPipeline.json
-    az datafactory pipeline create --factory-name $DataFactoryName --pipeline $DataFactoryPipelineDefinitionFileName --name $PipelineName --resource-group $ResourceGroupName
+    az datafactory pipeline create --factory-name $DataFactoryName --pipeline $DataFactoryPipelineDefinitionFileName --name $PipelineName --resource-group $DEVResourceGroupName
 }
 
 
