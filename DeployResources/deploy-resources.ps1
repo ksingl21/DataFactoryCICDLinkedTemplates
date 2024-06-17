f142d236ad0fa66cf64009756ec157a24827c992
Nick Ludwig <nick.ludwig223@gmail.com>
2024-06-17 17:07:24 -0500

    Updated cicd-pipeline testing purposes

--- DeployResources/deploy-resources.ps1
+++ DeployResources/deploy-resources.ps1
@@ -5,6 +5,17 @@
     # Links the Data Factory to a GitHub repo.
 
 
+    # https://learn.microsoft.com/en-us/cli/azure/storage/container?view=azure-cli-latest#az-storage-container-generate-sas
+    # https://andrewmatveychuk.com/how-to-deploy-linked-arm-templates-from-private-azure-devops-repositories/
+    # https://github.com/starkfell/100DaysOfIaC/blob/master/articles/day.42.deploy.nested.arm.templates.using.storage.accounts.in.yaml.pipeline.md?ref=andrewmatveychuk.com#1-preparing-the-arm-templates
+    # https://www.nathannellans.com/post/ci-cd-with-azure-data-factory-part-2
+    # https://github.com/nnellans/azure-data-factory/tree/main
+
+
+    # https://github.com/JFolberth/TheYAMLPipelineOne
+    # https://blog.johnfolberth.com/deploying-linked-arm-templates-via-yaml-pipelines/
+
+
 # Resource Names and Config
 $DEVResourceGroupName = 'rg-adf-cicd-linked'
 $UATResourceGroupName = 'rg-adf-cicd-linked-uat'
@@ -21,17 +32,17 @@ $GitHubRepoCollaborationBranchName = 'main'
 $GitHubRepoRootFolderName = '/'
 
 
-# Create the DEV, UAT, PROD resource group names
-az group create --name $DEVResourceGroupName --location $Location # DEV
-az group create --name $UATResourceGroupName --location $Location # UAT
-az group create --name $PRODResourceGroupName --location $Location # PROD
+# # Create the DEV, UAT, PROD resource group names
+# az group create --name $DEVResourceGroupName --location $Location # DEV
+# az group create --name $UATResourceGroupName --location $Location # UAT
+# az group create --name $PRODResourceGroupName --location $Location # PROD
 
 
-# Create a Data Factory
+# # Create a Data Factory
 az datafactory create --factory-name $DataFactoryName --resource-group $DEVResourceGroupName --location $Location
 
 
-# Creates 200 new Data Factory pipelines (35 Wait Activities in each pipeline). 
+# # Creates 200 new Data Factory pipelines (35 Wait Activities in each pipeline). 
 # To deploy fewer or more pipelines, change the -le 200 to a different number. Ex: -le 10 will deploy 10 pipelines.
 # Loops from 1 to 200 creating a pipeline named PL_WAIT_Number. Ex: PL_WAIT_1, PL_WAIT_2... PL_WAIT_200 
 for ($i = 1; $i -le 200; $i++) {
@@ -45,9 +56,10 @@ for ($i = 1; $i -le 200; $i++) {
 # Configure Data Factory to GitHub repo
 
 # Get the resource ID for the Data Factory. Ex: /subscriptions/xxxxxx/resourceGroups/xxxxx/providers/Microsoft.DataFactory/factories/datafactoryname
-$DataFactoryResourceID = $(az ad sp list --display-name $DataFactoryName --query "[].alternativeNames[1]" --output tsv)
+$DataFactoryResourceID = $(az ad sp list --filter "displayname eq '$DataFactoryName'" --query "[].alternativeNames[1]" --output tsv)
 
-# Configures the Data Factory to the GitHub repo
+
+# # Configures the Data Factory to the GitHub repo
 az datafactory configure-factory-repo --factory-git-hub-configuration host-name=$GitHubHostNameURL  account-name=$GitHubAccountName repository-name=$GitHubRepositoryName `
     collaboration-branch=$GitHubRepoCollaborationBranchName root-folder=$GitHubRepoRootFolderName --location $Location `
     --factory-resource-id $DataFactoryResourceID
