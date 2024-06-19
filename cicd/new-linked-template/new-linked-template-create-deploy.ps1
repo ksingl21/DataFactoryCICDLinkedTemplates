aa5de7da9a6374a34c5db3a90a753537676f0a0b
Nick Ludwig <nick.ludwig223@gmail.com>
2024-06-19 06:54:37 -0500

    Updated dev variables and added dynamic params for code

--- cicd/new-linked-template/new-linked-template-create-deploy.ps1
+++ cicd/new-linked-template/new-linked-template-create-deploy.ps1
@@ -3,20 +3,18 @@
 # https://stackoverflow.com/questions/47779157/convertto-json-and-convertfrom-json-with-special-characters
 # https://learn.microsoft.com/en-us/cli/azure/delete-azure-resources-at-scale#delete-all-azure-resources-of-a-type
 
-# az group delete -name $rgName --force-deletion-types Microsoft.Resources/templateSpecs --yes
 
-
-
+# Defining parameters for the script
 [CmdletBinding()]
 param(
-  $RootFolderPathLinkedARMTemplates,
-  $ResourceGroupName
+  $FolderPathADFLinkedARMTemplates,
+  $DeployTemplateSpecsResourceGroupName
 )
 
 
-$LinkedARMTemplateFiles = Get-ChildItem -Path $RootFolderPathLinkedARMTemplates -Exclude *master* # Excludes the master.json and parameters_master.json files
+$LinkedARMTemplateFiles = Get-ChildItem -Path $FolderPathADFLinkedARMTemplates -Exclude *master* # Excludes the master.json and parameters_master.json files
 
-    Write-Host "Attempting to create the template specs for the linked ARM templates. Template Spec resources will be deployed in Resource Group $ResourceGroupName. This may take a couple of mins."
+    Write-Host "Attempting to create the template specs for the linked ARM templates. Template Spec resources will be deployed in Resource Group $DeployTemplateSpecsResourceGroupName. This may take a couple of mins."
     Write-Host `n
 
     foreach ($FileName in $LinkedARMTemplateFiles.Name) {
@@ -26,16 +24,16 @@ $LinkedARMTemplateFiles = Get-ChildItem -Path $RootFolderPathLinkedARMTemplates 
       
       # Create a new Template Spec for each ARM Template. Doesn't update the ARM Template at all
       Write-Host "Attempting to create a new Template Spec for linked ARM template $TemplateSpecName.json"
-      az ts create --name $TemplateSpecName --version "1.0.0.0" --resource-group $ResourceGroupName --location 'eastus' --template-file $RootFolderPathLinkedARMTemplates/$FileName --yes --output none
+      az ts create --name $TemplateSpecName --version "1.0.0.0" --resource-group $DeployTemplateSpecsResourceGroupName --location 'eastus' --template-file $FolderPathADFLinkedARMTemplates/$FileName --yes --output none
       
       Write-Host "Successfully created a new Template Spec called $TemplateSpecName for linked ARM template $TemplateSpecName.json"
       Write-Host `n
     }
 
-    Write-Host "Successfully created all necessary Template Specs in Resource Group $ResourceGroupName"
+    Write-Host "Successfully created all necessary Template Specs in Resource Group $DeployTemplateSpecsResourceGroupName"
 
     Write-Host "Attempting to read the ArmTemplate_master.json file"
-    $ArmTemplateMasterFile = GET-CONTENT $RootFolderPathLinkedARMTemplates/ArmTemplate_master.json -Raw | ConvertFrom-Json
+    $ArmTemplateMasterFile = GET-CONTENT $FolderPathADFLinkedARMTemplates/ArmTemplate_master.json -Raw | ConvertFrom-Json
 
     # Remove the containerUri and containerSasToken parameters
     ($ArmTemplateMasterFile.parameters).PSObject.Properties.Remove('containerUri')
@@ -47,9 +45,7 @@ $LinkedARMTemplateFiles = Get-ChildItem -Path $RootFolderPathLinkedARMTemplates 
     $ResourceName = $item.Name -Match 'ArmTemplate_.*' # Extracts the Arm Template name out of the resource name property. Ex: my-datafactory-name_ArmTemplate_0 returns ArmTemplate_0
     $TemplateSpecExtractedName = $matches[0] # $matches is an automatic variable in PowerShell. https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_automatic_variables?view=powershell-7.4#matches
 
-    $TemplateSpecResourceID = $(az ts show --name $TemplateSpecExtractedName --resource-group $ResourceGroupName --version "1.0.0.0" --query "id")
-
-    # ($item.properties.templateLink.uri.Split(',')[1]).Trim().replace("'", "").replace('/', '')
+    $TemplateSpecResourceID = $(az ts show --name $TemplateSpecExtractedName --resource-group $DeployTemplateSpecsResourceGroupName --version "1.0.0.0" --query "id")
 
     $item.properties.templateLink | Add-Member -Name "id" -value $TemplateSpecResourceID.replace("`"","") -MemberType NoteProperty # removes the initial and ending double quotes from the string
     ($item.properties.templateLink).PSObject.Properties.Remove('uri')
@@ -57,10 +53,8 @@ $LinkedARMTemplateFiles = Get-ChildItem -Path $RootFolderPathLinkedARMTemplates 
 
     $item.apiVersion = '2019-11-01'
 
-    # $item.apiVersion
-    # Write-Host $item.properties.templateLink
-    # Write-Host $item.properties
     }
+
 
     Write-Host "Attempting to output the new Master.json file"
 
@@ -70,6 +64,6 @@ $LinkedARMTemplateFiles = Get-ChildItem -Path $RootFolderPathLinkedARMTemplates 
     [Regex]::Replace($_, 
         "\\u(?<Value>[a-zA-Z0-9]{4})", {
             param($m) ([char]([int]::Parse($m.Groups['Value'].Value,
-                [System.Globalization.NumberStyles]::HexNumber))).ToString() } )} |  Set-Content 'NewARMTemplateV2_master.json' # -Path '$RootFolderPathLinkedARMTemplates'
+                [System.Globalization.NumberStyles]::HexNumber))).ToString() } )} |  Set-Content 'NewARMTemplateV2_master.json'
 
     Write-Host "Successfully created the NewARMTemplateV2_master.json file"
