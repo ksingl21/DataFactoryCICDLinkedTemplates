145439c132eb42d82f55b39b44b018498970a5f7
Nick Ludwig <nick.ludwig223@gmail.com>
2024-06-22 08:56:20 -0500

    Updated powershell info for master file

--- cicd/new-linked-template/new-linked-template-create-deploy.ps1
+++ cicd/new-linked-template/new-linked-template-create-deploy.ps1
@@ -19,9 +19,9 @@ Step 2:
         - Removes the uri and contentVersion properties
     - Updates the apiVersion property to one that can use the Template Spec id property (2019-11-01 for example) 
     - Ensures the special characters in JSON are escaped properly when generating the updated file (see https://stackoverflow.com/questions/47779157/convertto-json-and-convertfrom-json-with-special-characters)
-    - Outputs the new file (doesn't overwrite the existing file) to the root of the repository: "$(Build.Repository.LocalPath)/NewARMTemplateV2_master.json"
+    - Outputs the new file (doesn't overwrite the existing file) to the root of the repository: Ex: "$(Build.Repository.LocalPath)/NewARMTemplateV2_master.json"
 
-    
+
 # Helpful Links:
 # https://dev.to/adbertram/running-powershell-scripts-in-azure-devops-pipelines-2-of-2-3j0e
 # https://stackoverflow.com/questions/47779157/convertto-json-and-convertfrom-json-with-special-characters
@@ -32,7 +32,7 @@ Step 2:
 #>
 
 
-# Defining parameters for the script
+# Defining parameters for the script. The values are passed in from the cicd-pipeline.yml pipeline (Create Template Specs for ADF Linked ARM Templates) task
 [CmdletBinding()]
 param(
   $FolderPathADFLinkedARMTemplates,
@@ -43,10 +43,10 @@ param(
   $OutputFolderPathNewADFMasterARMTemplate
 )
 
-# Grabs the ADF linked template files 
-$LinkedARMTemplateFiles = Get-ChildItem -Path $FolderPathADFLinkedARMTemplates -Exclude *master* # Excludes the master.json and parameters_master.json files
+# Grabs the ADF linked template files
+$LinkedARMTemplateFiles = Get-ChildItem -Path $FolderPathADFLinkedARMTemplates -Exclude *master* # Excludes the ArmTemplate_master.json and ArmTemplateParameters_master.json files
 
-    Write-Host "Attempting to create the template specs for the linked ARM templates. Template Spec resources will be deployed in Resource Group $DeployTemplateSpecsResourceGroupName. This may take a few of minutes."
+    Write-Host "Attempting to create the Template Specs for the Linked ARM Templates. Template Spec resources will be deployed in Resource Group $DeployTemplateSpecsResourceGroupName. This may take a few of minutes."
     Write-Host `n
 
     foreach ($FileName in $LinkedARMTemplateFiles.Name) {
@@ -59,7 +59,7 @@ $LinkedARMTemplateFiles = Get-ChildItem -Path $FolderPathADFLinkedARMTemplates -
       az ts create --name $TemplateSpecName --version $TemplateSpecsVersionNumber --resource-group $DeployTemplateSpecsResourceGroupName --location $DeployTemplateSpecsResourceGroupLocation `
         --template-file $FolderPathADFLinkedARMTemplates/$FileName --yes --output none # --yes means don't prompt for confirmation and overwrite the existing Template Spec if it exists
       
-      Write-Host "Successfully created a new Template Spec called $TemplateSpecName for linked ARM template $TemplateSpecName.json"
+      Write-Host "Successfully created a new Template Spec called $TemplateSpecName for Linked ARM Template $TemplateSpecName.json"
       Write-Host `n
     }
 
@@ -76,19 +76,19 @@ $LinkedARMTemplateFiles = Get-ChildItem -Path $FolderPathADFLinkedARMTemplates -
     ($MasterARMTemplateFile.parameters).PSObject.Properties.Remove('containerSasToken')
 
     
-    foreach ($item in $MasterARMTemplateFile.resources) {
+    foreach ($Resource in $MasterARMTemplateFile.resources) {
 
-    $ResourceName = $item.Name -Match 'ArmTemplate_.*' # Extracts the ARM Template name out of the resource name property. Ex: my-datafactory-name_ArmTemplate_0 returns ArmTemplate_0
+    $ResourceName = $Resource.Name -Match 'ArmTemplate_.*' # Extracts the ARM Template name out of the resource name property. Ex: my-datafactory-name_ArmTemplate_0 returns ArmTemplate_0
     $TemplateSpecExtractedName = $matches[0] # $matches is an automatic variable in PowerShell. https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_automatic_variables?view=powershell-7.4#matches
 
     $TemplateSpecResourceID = $(az ts show --name $TemplateSpecExtractedName --resource-group $DeployTemplateSpecsResourceGroupName --version $TemplateSpecsVersionNumber --query "id")
 
-    $item.properties.templateLink | Add-Member -Name "id" -value $TemplateSpecResourceID.replace("`"","") -MemberType NoteProperty # removes the initial and ending double quotes from the string
-    ($item.properties.templateLink).PSObject.Properties.Remove('uri')
-    ($item.properties.templateLink).PSObject.Properties.Remove('contentVersion')
+    $Resource.properties.templateLink | Add-Member -Name "id" -value $TemplateSpecResourceID.replace("`"","") -MemberType NoteProperty # removes the initial and ending double quotes from the string
+    ($Resource.properties.templateLink).PSObject.Properties.Remove('uri')
+    ($Resource.properties.templateLink).PSObject.Properties.Remove('contentVersion')
 
-    # Updates the API version to one that can use the TemplateSpec ID
-    $item.apiVersion = '2019-11-01'
+    # Updates the API version to one that can use the Template Spec ID
+    $Resource.apiVersion = '2019-11-01'
     }
 
 
@@ -100,11 +100,12 @@ $LinkedARMTemplateFiles = Get-ChildItem -Path $FolderPathADFLinkedARMTemplates -
     [Regex]::Replace($_, 
         "\\u(?<Value>[a-zA-Z0-9]{4})", {
             param($m) ([char]([int]::Parse($m.Groups['Value'].Value,
-                [System.Globalization.NumberStyles]::HexNumber))).ToString() } )} |  Set-Content 'NewARMTemplateV2_master.json'
+                [System.Globalization.NumberStyles]::HexNumber))).ToString() } )} |  Set-Content "$TemplateSpecMasterName.json"
 
     Write-Host "Successfully created the $TemplateSpecMasterName.json file"
     
     Write-Host "Attempting to create the Template Spec for the $TemplateSpecMasterName.json file"
+    
     az ts create --name $TemplateSpecMasterName --version $TemplateSpecsVersionNumber --resource-group $DeployTemplateSpecsResourceGroupName --location $DeployTemplateSpecsResourceGroupLocation `
       --template-file "$OutputFolderPathNewADFMasterARMTemplate/$TemplateSpecMasterName.json" --output none
     
